b75458702f39d640a38c7a946405e7ac130515f7
Isaac Pascoe <impascoe@pascalax.com>
2026-05-06 13:47:09 +0800

    feat(main): add tty detection and refactor process spawning

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,14 @@ pub fn main(init: std.process.Init) !void {
     var environ_map = try init.minimal.environ.createMap(gpa);
     defer environ_map.deinit();
 
+    var stdin = std.Io.File.stdin();
+    // var stdinreader = std.Io.File.stdin().reader(io, &in_buf);
+
     var stdout = std.Io.File.stdout().writer(io, &out_buf);
+
+    const isatty = stdin.isTty(io);
+
+    try stdout.interface.print("isatty: {any}\n", .{isatty});
 
     for (environ_map.keys(), environ_map.values()) |key, value| {
         try stdout.interface.print("env: {s}={s}\n", .{ key, value });
@@ -65,24 +72,15 @@ pub fn main(init: std.process.Init) !void {
             continue;
         }
 
-        if (std.process.run(gpa, io, .{ .argv = argv.items })) |result| {
-            defer gpa.free(result.stdout);
-            defer gpa.free(result.stderr);
-
-            stdout.interface.print("{s}", .{result.stdout}) catch {};
-            stdout.interface.flush() catch {};
-
-            if (result.stderr.len != 0) {
-                stdout.interface.print("{s}", .{result.stderr}) catch {};
-                stdout.interface.flush() catch {};
+        var child: std.process.Child = std.process.spawn(io, .{ .argv = argv.items, .environ_map = &environ_map }) catch |err| {
+            switch (err) {
+                error.FileNotFound => stdout.interface.print("pash: command not found: {s}\n", .{argv.items[0]}) catch {},
+                else => stdout.interface.print("{any}", .{err}) catch {},
             }
-        } else |err| switch (err) {
-            error.FileNotFound => {
-                stdout.interface.print("pash: command not found: {s}\n", .{argv.items[0]}) catch {};
-                stdout.interface.flush() catch {};
-            },
-            else => stdout.interface.print("{any}", .{err}) catch {},
-        }
+            stdout.interface.flush() catch {};
+            continue;
+        };
+        _ = child.wait(io) catch {};
     }
     stdout.interface.flush() catch {};
 }
