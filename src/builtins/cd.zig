3f35e4fb53e4b82273b6062ee452282012b40ca6
Isaac Pascoe <impascoe@pascalax.com>
2026-06-06 21:02:28 +0800

    feat: add exit status tracking and error output to stderr
    - Change `cd` function return type from `void` to `u8` to return exit
      status
    - Add stderr writer to main and use it for error messages instead of
      stdout
    - Track last command exit status in `last_status` variable
    - Initialize PWD environment variable if not set
    - Defer path printing in `cd` until after successful directory change
    - Return appropriate exit codes (0 for success, 1 for failure) from `cd`
    - Capture and return child process exit codes in main loop
    - Fix error message output streams throughout codebase

--- src/builtins/cd.zig
+++ src/builtins/cd.zig
@@ -2,24 +2,39 @@ const std = @import("std");
 
 const flags = enum { L_FLAG, P_FLAG };
 
-pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map, argv: [][]const u8) !void {
+pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map, argv: [][]const u8) !u8 {
     var stdout_buf: [4096]u8 = undefined;
+    var err_buf: [4096]u8 = undefined;
+
+    var print_path: bool = false;
+
     var cdpath_path: ?[]u8 = null;
     defer if (cdpath_path) |path| gpa.free(path);
 
     var stdout = std.Io.File.stdout().writer(io, &stdout_buf);
-    const current_dir = environ.get("PWD") orelse return;
+    var stderr = std.Io.File.stderr().writer(io, &err_buf);
+
+    if (environ.get("PWD") == null) {
+        const cwd = try std.process.currentPathAlloc(io, gpa);
+        defer gpa.free(cwd);
+        try environ.put("PWD", cwd);
+    }
+
+    const current_dir = environ.get("PWD").?;
 
     if (argv.len < 2) {
         const home_dir = environ.get("HOME") orelse {
-            try stdout.interface.print("pash: cd: $HOME not set \n", .{});
-            try stdout.interface.flush();
-            return;
+            try stderr.interface.print("pash: cd: $HOME not set \n", .{});
+            try stderr.interface.flush();
+            return 1;
         };
 
-        if (std.Io.Threaded.chdir(home_dir)) {} else |err| {
-            try stdout.interface.print("cd: {s}: {any}\n", .{ home_dir, err });
-            try stdout.interface.flush();
+        if (std.Io.Threaded.chdir(home_dir)) {
+            return 0;
+        } else |err| {
+            try stderr.interface.print("cd: {s}: {any}\n", .{ home_dir, err });
+            try stderr.interface.flush();
+            return 1;
         }
     } else {
         var flag = flags.L_FLAG;
@@ -29,12 +44,11 @@ pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map,
         if (std.mem.startsWith(u8, target, "-")) {
             if (std.mem.eql(u8, "-", target)) {
                 const prev_dir = environ.get("OLDPWD") orelse {
-                    return;
+                    return 1;
                 };
 
                 curpath = prev_dir;
-                try stdout.interface.print("{s}\n", .{curpath});
-                try stdout.interface.flush();
+                print_path = true;
             } else if (!(argv.len < 3)) {
                 if (std.mem.endsWith(u8, target, "P")) {
                     flag = flags.P_FLAG;
@@ -46,17 +60,17 @@ pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map,
                     curpath = target;
                 }
             } else {
-                try stdout.interface.print("pash: cd: No file or directory specified\n", .{});
-                try stdout.interface.flush();
-                return;
+                try stderr.interface.print("pash: cd: No file or directory specified\n", .{});
+                try stderr.interface.flush();
+                return 1;
             }
         }
 
         if (std.mem.eql(u8, "~", target)) {
             const home_dir = environ.get("HOME") orelse {
-                try stdout.interface.print("pash: cd: $HOME not set \n", .{});
-                try stdout.interface.flush();
-                return;
+                try stderr.interface.print("pash: cd: $HOME not set \n", .{});
+                try stderr.interface.flush();
+                return 1;
             };
             curpath = home_dir;
         } else if (std.mem.startsWith(u8, target, "/")) {
@@ -66,8 +80,9 @@ pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map,
 
             if (cdpath_path) |path| {
                 curpath = path;
-                try stdout.interface.print("{s}\n", .{curpath});
-                try stdout.interface.flush();
+                print_path = true;
+                // try stdout.interface.print("{s}\n", .{curpath});
+                // try stdout.interface.flush();
             }
         }
 
@@ -77,46 +92,53 @@ pub fn cd(io: std.Io, gpa: std.mem.Allocator, environ: *std.process.Environ.Map,
         }
 
         if (std.Io.Threaded.chdir(curpath)) {
+            if (print_path) {
+                try stdout.interface.print("{s}\n", .{curpath});
+                try stdout.interface.flush();
+            }
+
             try environ.put("OLDPWD", current_dir);
 
             const path = try std.process.currentPathAlloc(io, gpa);
             defer gpa.free(path);
 
             try environ.put("PWD", path);
+            return 0;
         } else |err| {
             switch (err) {
                 error.NotDir => {
-                    try stdout.interface.print("pash: cd: '{s}'  No such file or directory\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}'  No such file or directory\n", .{target});
                 },
                 error.SymLinkLoop => {
-                    try stdout.interface.print("pash: cd: '{s}' Too many levels of symbolic links\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' Too many levels of symbolic links\n", .{target});
                 },
                 error.SystemResources => {
-                    try stdout.interface.print("pash: cd: '{s}' Cannot allocate memory to process\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' Cannot allocate memory to process\n", .{target});
                 },
                 error.NameTooLong => {
-                    try stdout.interface.print("pash: cd: '{s}' File name is too long\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' File name is too long\n", .{target});
                 },
                 error.FileNotFound => {
-                    try stdout.interface.print("pash: cd: '{s}' File or directory not found\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' File or directory not found\n", .{target});
                 },
                 error.FileSystem => {
-                    try stdout.interface.print("pash: cd: '{s}' I/O operation failed\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' I/O operation failed\n", .{target});
                 },
                 error.BadPathName => {
-                    try stdout.interface.print("pash: cd: '{s}' Illegal byte sequence encountered\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' Illegal byte sequence encountered\n", .{target});
                 },
                 error.Canceled => {
-                    try stdout.interface.print("pash: cd: Process has been cancelled\n", .{});
+                    try stderr.interface.print("pash: cd: Process has been cancelled\n", .{});
                 },
                 error.AccessDenied => {
-                    try stdout.interface.print("pash: cd: '{s}' Permission denied\n", .{target});
+                    try stderr.interface.print("pash: cd: '{s}' Permission denied\n", .{target});
                 },
                 error.Unexpected => {
-                    try stdout.interface.print("pash: cd: Unknown error encountered\n", .{});
+                    try stderr.interface.print("pash: cd: Unknown error encountered\n", .{});
                 },
             }
-            try stdout.interface.flush();
+            try stderr.interface.flush();
+            return 1;
         }
     }
 }
--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ pub fn main(init: std.process.Init) !void {
 
     var in_buf: [4096]u8 = undefined;
     var out_buf: [4096]u8 = undefined;
+    var err_buf: [4096]u8 = undefined;
 
     var environ_map = try init.minimal.environ.createMap(gpa);
     defer environ_map.deinit();
@@ -16,8 +17,11 @@ pub fn main(init: std.process.Init) !void {
     // var stdinreader = std.Io.File.stdin().reader(io, &in_buf);
 
     var stdout = std.Io.File.stdout().writer(io, &out_buf);
+    var stderr = std.Io.File.stderr().writer(io, &err_buf);
 
     const isatty = stdin.isTty(io);
+
+    var last_status: u8 = 0;
 
     try stdout.interface.print("isatty: {any}\n", .{isatty});
 
@@ -37,13 +41,13 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         const res = readCommand(io, &in_buf, &out_buf) catch |err| switch (err) {
             error.StreamTooLong => {
-                stdout.interface.print("pash: input line too long\n", .{}) catch {};
-                stdout.interface.flush() catch {};
+                stderr.interface.print("pash: input line too long\n", .{}) catch {};
+                stderr.interface.flush() catch {};
                 continue;
             },
             else => {
-                stdout.interface.print("{any}", .{err}) catch {};
-                stdout.interface.flush() catch {};
+                stderr.interface.print("{any}", .{err}) catch {};
+                stderr.interface.flush() catch {};
                 break;
             },
         };
@@ -55,8 +59,8 @@ pub fn main(init: std.process.Init) !void {
 
         var it = std.mem.tokenizeAny(u8, trimmed, " \t");
         while (it.next()) |tok| argv.append(gpa, tok) catch {
-            stdout.interface.print("pash: out of memory\n", .{}) catch {};
-            stdout.interface.flush() catch {};
+            stderr.interface.print("pash: out of memory\n", .{}) catch {};
+            stderr.interface.flush() catch {};
             continue;
         };
 
@@ -68,19 +72,33 @@ pub fn main(init: std.process.Init) !void {
         }
 
         if (std.mem.eql(u8, argv.items[0], "cd")) {
-            cd(io, gpa, &environ_map, argv.items) catch {};
+            last_status = cd(io, gpa, &environ_map, argv.items) catch |err| blk: {
+                stderr.interface.print("pash: cd: {any}", .{err}) catch {};
+                stderr.interface.flush() catch {};
+                break :blk 1;
+            };
             continue;
         }
 
         var child: std.process.Child = std.process.spawn(io, .{ .argv = argv.items, .environ_map = &environ_map }) catch |err| {
             switch (err) {
-                error.FileNotFound => stdout.interface.print("pash: command not found: {s}\n", .{argv.items[0]}) catch {},
-                else => stdout.interface.print("{any}", .{err}) catch {},
+                error.FileNotFound => stderr.interface.print("pash: command not found: {s}\n", .{argv.items[0]}) catch {},
+                else => stderr.interface.print("{any}", .{err}) catch {},
             }
-            stdout.interface.flush() catch {};
+            stderr.interface.flush() catch {};
             continue;
         };
-        _ = child.wait(io) catch {};
+        const term = child.wait(io) catch |err| {
+            stderr.interface.print("pash: wait failed: {any}", .{err}) catch {};
+            stderr.interface.flush() catch {};
+            last_status = 1;
+            continue;
+        };
+
+        last_status = switch (term) {
+            .exited => |exit_code| @intCast(exit_code),
+            else => 1,
+        };
     }
     stdout.interface.flush() catch {};
 }
